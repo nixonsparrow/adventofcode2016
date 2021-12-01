97527438ee0074a9050f67bcc3bd1c4308351913
Nikodem Wróbel <nixonsparrow@gmail.com>
2021-12-01 18:58:17 +0100

    Added bunny jumper tests (day1)

--- tests/test_day1.py
+++ tests/test_day1.py
@@ -2,6 +2,16 @@ from ..puzzles import day1
 
 
 class TestDay1:
+    def test_bunny_jumper(self):
+        assert day1.bunny_jumper(['L4']) == 4
+        assert day1.bunny_jumper(['L4'], full_course=False) == 4
+        assert day1.bunny_jumper(['R4', 'L5', 'L1', 'L3']) == 5
+        assert day1.bunny_jumper(['L4', 'L4', 'L2', 'L6']) == 4
+        assert day1.bunny_jumper(['L4', 'L4', 'L2', 'L6'], full_course=False) == 2
+
+    def test_ultimate_bunny_jumper(self):
+        assert day1.bunny_jumper([f'L{x}' for x in range(999)]) == 999
+        assert day1.bunny_jumper([f'{"R" if x%2==0 else "L"}{x}' for x in range(999)]) == 498501
 
     def test_part1(self):
         assert day1.part1('/../inputs/day1_1_test1.txt') == 5
