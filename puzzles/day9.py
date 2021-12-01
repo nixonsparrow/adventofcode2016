7f178fa9fcc19f1cb55e7ef3d80619b43b7b1b56
Nikodem Wróbel <nixonsparrow@gmail.com>
2021-12-01 12:01:34 +0100

    prepare Day 9 to code with tdd

--- puzzles/day9.py
+++ puzzles/day9.py
@@ -1,2 +1,25 @@
+from os import path
+
+
+def txt_opener(input_file):
+    return list(map(str, open(path.dirname(__file__) + input_file).read().split('\n')))
+
+
+def decompressor(file):
+    dec_file = file
+    return dec_file
+
+
+def part1(input_file=''):
+    final_input = txt_opener(input_file)
+    decompressed_file = decompressor(final_input)
+    return len(decompressed_file)
+
+
+def part2(input_file=''):
+    final_input = list(map(str, open(path.dirname(__file__) + input_file).read().split('\n')))
+    return False
+
+
 if __name__ == '__main__':
-    pass
+    print([input for input in txt_opener('/../inputs/day9_test.txt')])
--- tests/test_day9.py
+++ tests/test_day9.py
@@ -3,10 +3,16 @@ import pytest
 
 
 class TestDay9:
-    pytestmark = pytest.mark.ignored
+    @pytest.mark.parametrize(('a', 'b'),
+        zip([input for input in day9.txt_opener('/../inputs/day9_test.txt')],
+        (6, 7, 9, 11, 6, 18)))
+    def test_decompressor(self, a, b):
+        assert len(day9.decompressor(a)) == b
 
+    @pytest.mark.ignored
     def test_part1(self):
-        assert 2 == 2
+        assert day9.part1() == 1
 
+    @pytest.mark.ignored
     def test_part2(self):
-        assert 2 == 22
+        assert day9.part2() is True
