e92d39a176fdbf7fac8178e5ec6f801fa6341346
Nikodem Wróbel <nixonsparrow@gmail.com>
2021-12-01 18:56:44 +0100

    Day 9 finished, last commit before merge

--- puzzles/day9.py
+++ puzzles/day9.py
@@ -7,25 +7,30 @@ def txt_opener(input_file):
     return the_list if len(the_list) > 1 else the_list[0]
 
 
-def command_executor(sequence):
-    processed_sequence = sequence
-    return processed_sequence
-
-
-def decompressor(old_sequence):
-    new_sequence = ''
+def decompressor(old_sequence, count_max=False):
+    new_sequence = '' if not count_max else [1 for _ in old_sequence]
+    algorithm_sum = 0
     i = 0
     while i < len(old_sequence):
         if old_sequence[i] != '(':
-            new_sequence += old_sequence[i]
+            if not count_max:
+                new_sequence += old_sequence[i]
+            else:
+                algorithm_sum += new_sequence[i]
             i += 1
         else:
             command = re.search(r'\d+x\d+', old_sequence[i:]).group()
             cmd_length, length, multiplier = len(command) + 2, int(command.split('x')[0]), int(command.split('x')[1])
-            new_sequence += old_sequence[sum((i, cmd_length)): sum((i, cmd_length, length))] * multiplier
-            i += cmd_length + length
 
-    return new_sequence
+            if not count_max:
+                new_sequence += old_sequence[sum((i, cmd_length)): sum((i, cmd_length, length))] * multiplier
+                i += cmd_length + length
+            else:
+                for char in range(sum([i, cmd_length]), sum([i, cmd_length, length])):
+                    new_sequence[char] *= multiplier
+                i += cmd_length
+
+    return new_sequence if not count_max else algorithm_sum
 
 
 def part1(input_file=''):
@@ -35,9 +40,10 @@ def part1(input_file=''):
 
 
 def part2(input_file=''):
-    final_input = list(map(str, open(path.dirname(__file__) + input_file).read().split('\n')))
-    return False
+    final_input = txt_opener(input_file)
+    result = decompressor(final_input, count_max=True)
+    return result
 
 
 if __name__ == '__main__':
-    print(decompressor('A(1x5)BC'))
+    print(decompressor('X(8x2)(3x3)ABCY', count_max=True))
--- tests/test_day9.py
+++ tests/test_day9.py
@@ -4,20 +4,28 @@ import pytest
 
 class TestDay9:
     @pytest.mark.parametrize(('sequence', 'length'),
-        zip([input for input in day9.txt_opener('/../inputs/day9_test.txt')],
+        zip([input for input in day9.txt_opener('/../inputs/day9_1_test.txt')],
         (6, 7, 9, 11, 6, 18)))
-    def test_decompressor(self, sequence, length):
+    def test_examples(self, sequence, length):
         assert len(day9.decompressor(sequence)) == length
 
+    def test_decompressor(self):
+        assert day9.decompressor('PyTest') == 'PyTest'
+        assert day9.decompressor('A_(1x1)Test') == 'A_Test'
+        assert day9.decompressor('A_(2x2)Te(2x2)st') == 'A_TeTestst'
+        assert day9.decompressor('A_(4x2)Test') == 'A_TestTest'
+
     @pytest.mark.parametrize(('filename', '_type'), zip(
-                             ('/../inputs/day9_test.txt', '/../inputs/day9_final.txt'),
-                             ('list', 'str')))
+                             ('/../inputs/day9_1_test.txt', '/../inputs/day9_final.txt'),
+                             (list, str)))
     def test_txt_opener(self, filename, _type):
-        assert type(day9.txt_opener(filename)).__name__ == _type
+        assert type(day9.txt_opener(filename)) == _type
 
     def test_part1(self):
         assert day9.part1('/../inputs/day9_final.txt') == 112830
 
-    @pytest.mark.ignored
-    def test_part2(self):
-        assert day9.part2() is True
+    @pytest.mark.parametrize(('filename', 'result'), [('/../inputs/day9_final.txt', 10931789799),
+        ('/../inputs/day9_2_test1.txt', 9), ('/../inputs/day9_2_test2.txt', 20),
+        ('/../inputs/day9_2_test3.txt', 241920), ('/../inputs/day9_2_test4.txt', 445)])
+    def test_part2(self, filename, result):
+        assert day9.part2(filename) == result
